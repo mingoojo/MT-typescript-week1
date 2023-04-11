cadacbe05c94dbfae5148858454da452adda706a
(MJ) <(jomg6696@gmail.com)>
2023-04-11 19:30:42 +0900

    modify:setting

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+ return(
+  <div>
+   hello world!
+  </div>
+ )
+}
+
+function main() {
+  const element = document.getElementById('root');
+
+  if (!element) {
+    return;
+  }
+
+  const root = ReactDOM.createRoot(element);
+
+  root.render((<App />));
+}
+
+main();
